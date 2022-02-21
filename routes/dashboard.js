3c6b5285fcde7f17d59250a9dd1debc2d26891d7
andronedev <andronedev@gmail.com>
2022-02-21 20:26:03 +0100

    up

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var db = require('../database/index.js');
 var path = require('path');
+var fs = require('fs');
 // access to the database (model is defined in models
 
 /* GET home page. */
@@ -25,7 +26,6 @@ router.get('/', async function (req, res, next) {
         ]
     });
     var nbImages = await db.models.Images.count();
-
 
     res.render('dashboard_index', { nbUsers: nbUsers, nbDevices: nbDevices, nbImages: nbImages, user, devices: devices, latestImages: latestImages });
 });
@@ -89,7 +89,18 @@ router.get('/devices/:id', async function (req, res, next) {
             ['createdAt', 'DESC']
         ]
     });
-    res.render('dashboard_device', { user, device: device, images: images });
+    var timelapses = await db.models.Timelapses.findAll({
+        where: {
+            userId: user.id,
+            deviceId: device.id
+        },
+
+        order: [
+            ['createdAt', 'DESC']
+        ]
+    });
+
+    res.render('dashboard_device', { user, device: device, images: images, timelapses: timelapses });
 });
 router.get('/devices/:id/script/:type', async function (req, res, next) {
     var user = req.session.loggedIn ? await db.models.Users.findOne({
@@ -139,8 +150,8 @@ router.post("/devices/:id/edit", async function (req, res, next) {
     if (req.body.name) {
         device.name = req.body.name;
     }
-    if (req.body.intervalS){
-        if (req.body.intervalS >= 5){
+    if (req.body.intervalS) {
+        if (req.body.intervalS >= 5) {
             device.intervalMs = req.body.intervalS * 1000;
         }
     }
@@ -167,9 +178,10 @@ router.post("/devices/:id/createtimelaps", async function (req, res, next) {
         res.redirect('/dashboard');
         next();
     }
-    var images = await db.models.images.findAll({
+    var images = await db.models.Images.findAll({
         where: {
-            deviceId: device.id
+            deviceId: device.id,
+            userId: user.id
         }
     });
     framerate = req.body.framerate;
@@ -180,11 +192,29 @@ router.post("/devices/:id/createtimelaps", async function (req, res, next) {
         res.redirect('/dashboard/devices/' + device.id);
         next();
     }
+    // delete all timelapses for this device
+    await db.models.Timelapses.findAll({
+        where: {
+            deviceId: device.id,
+            userId: user.id
+        }
+    }).then(async function (timelapses) {
+        for (var i = 0; i < timelapses.length; i++) {
+            fs.unlink(path.join(__dirname, '../timelapses/output/' + timelapses[i].url), function (err) {
+                if (err) {
+                    console.log(err);
+                }
+            });
+            await timelapses[i].destroy();
+        }
+    });
+
+
     require("../timelaps/generator.js")(images, framerate, user.id, device.id)
     res.redirect('/dashboard/devices/' + device.id);
 });
 
-router.get('/devices/:id/timelaps/:tid', async function (req, res, next) {
+router.get('/timelaps/:tid', async function (req, res, next) {
     var user = req.session.loggedIn ? await db.models.Users.findOne({
         where: {
             id: req.session.userid
@@ -194,18 +224,19 @@ router.get('/devices/:id/timelaps/:tid', async function (req, res, next) {
         res.redirect('/users/login');
         next();
     }
-    var timeLaps = await db.models.TimeLaps.findOne({
+    var timeLaps = await db.models.Timelapses.findOne({
         where: {
             id: req.params.tid,
             userId: user.id,
-            deviceId: req.params.id
         }
     });
     if (!timeLaps) {
         res.redirect('/dashboard/devices/' + req.params.id);
         next();
     }
-    res.sendFile(path.join(__dirname, '../timelaps/' + timeLaps.id + '.mp4'));
+    // send mp4
+    res.header('Content-Type', 'video/mp4'); 
+    res.sendFile(path.join(__dirname, '../timelaps/output/' + timeLaps.url));
 });
 
 
@@ -228,7 +259,6 @@ router.get('/image/:id', async function (req, res, next) {
     if (!image) {
         res.sendStatus(404);
     }
-    console.log(image)
 
     res.sendFile(path.join(__dirname, '../images/' + image.url));
 
--- timelaps/generator.js
+++ timelaps/generator.js
@@ -2,9 +2,13 @@ var pathToFfmpeg = require('ffmpeg-static');
 const util = require('util');
 const exec = util.promisify(require('child_process').exec);
 var db = require('../database/index.js');
+const path = require('path');
+const fs = require('fs');
 async function createTimeLaps(images, framerate, userid, deviceid) {
+    framerate = parseInt(framerate);
     // create temp file with images 
-    let output = "output-"+Math.random().toString(36).substring(2, 15)+".mp4";
+    console.log("createTimeLaps")
+    let output = "output-" + Math.random().toString(36).substring(2, 15) + ".mp4";
     var timelaps = await db.models.Timelapses.create({
         name: "",
         userId: userid,
@@ -15,33 +19,25 @@ async function createTimeLaps(images, framerate, userid, deviceid) {
         framerate: framerate,
         url: output,
         logs: ""
+    }).catch(err => {
+        console.error(err);
     });
 
+    output = path.join(__dirname, '../timelaps/output/' + output);
+
     let source = "";
-    for(let i = 0; i < images.length; i++){
-        source += "file '" + images[i].url + "'\n";
+    for (let i = 0; i < images.length; i++) {
+        source += "file '" + path.join(__dirname, "../images/" + images[i].url) + "'\n";
     }
     let tempFile = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15) + ".txt";
-    
+
     fs.writeFileSync(tempFile, source);
-    
-    await exec(`${pathToFfmpeg} -y -f concat -safe 0 -i ${tempFile} -framerate ${framerate} -c:v libx264 -pix_fmt yuv420p -r 30 -vf "scale=trunc(iw/2)*2:trunc(ih/2)*2" -preset ultrafast -crf 0 -c:a copy ${output}.mp4`, (error, stdout, stderr) => {
-        timelaps.update({
-            logs: stdout
-        });
-        if (error) {
-            timelaps.update({
-                status: "erreur",
-                logs: stderr
-            });
-            console.error(`exec error: ${error}`);
-            return;
-        }else {
-            timelaps.update({
-                status: "terminé"
-            });
-        }
-    });
+
+    var job = await exec(`${pathToFfmpeg} -y -f concat -safe 0 -i ${tempFile} -framerate ${framerate} -c:v libx264 -pix_fmt yuv420p -r 30 -vf "scale=trunc(iw/2)*2:trunc(ih/2)*2" -preset ultrafast -crf 0 -c:a copy ${output}`)
+
+    timelaps.logs = job.stdout;
+    timelaps.status = "terminé";
+    timelaps.save();
 
     fs.unlinkSync(tempFile);
 
